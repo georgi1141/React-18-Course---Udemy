e4646ee46d09d6dcabff5b018ad0a53486638b0a
georgi1141 <g.zh.yanev@gmail.com>
2023-10-29 14:18:53 +0100

    useEffect challange completed

--- 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,46 @@
+import { useState } from "react";
+import data from "./data";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+
+  function removePerson(id) {
+    setPeople(people.filter((person) => person.id !== id));
+  }
+
+  return (
+    <div className="container">
+      <h1>{people.length} birthdays today</h1>
+
+      {people.map((person) => {
+        return (
+          <article key={person.id} className="person">
+            <img src={person.image} alt={person.image} className="img" />
+            <div>
+              <h4>{person.name}</h4>
+              <p>{person.age} years</p>
+            </div>
+            <button
+              className="btn"
+              style={{ marginTop: "1em" }}
+              onClick={() => {
+                removePerson(person.id);
+              }}
+            >
+              remove
+            </button>
+          </article>
+        );
+      })}
+      <button
+        className="btn"
+        onClick={() => {
+          setPeople([]);
+        }}
+      >
+        Clear All
+      </button>
+    </div>
+  );
 };
 export default App;
