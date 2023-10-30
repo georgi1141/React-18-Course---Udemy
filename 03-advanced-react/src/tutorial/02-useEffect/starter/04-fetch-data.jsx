1e591a70039100778643d6caa5aef09ee7c045e8
georgi1141 <g.zh.yanev@gmail.com>
2023-10-30 08:04:31 +0100

    useEffect hook covered

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,41 @@
+import { useEffect,useState } from "react";
+
 const url = 'https://api.github.com/users';
 
+
+
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+
+  const [users,setUsers] = useState([])
+
+
+  useEffect(() => {
+    async function getUsers(){
+      try {
+
+        const response = await fetch(url);
+          const data = await response.json();
+          setUsers(data);
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    getUsers()
+}, []);
+
+
+
+
+  return <div style={{display:'flex', flexDirection:'column' ,justifyContent:'center',alignItems:'center'}}>
+    <h4 >github users</h4>
+    {users.map((user,index)=>{
+      return <div key={index} >
+      <img style={{display:'block', width:'10em',}} src={user.avatar_url} alt="pic"/>
+      <p key={index}>{user.login}</p>
+      </div>
+
+
+    })}
+  </div>
 };
 export default FetchData;
