3408a47790e429ef15a1f989ab3e306ba860d031
georgi1141 <g.zh.yanev@gmail.com>
2023-11-01 17:10:03 +0100

    controlled input + challange completed

--- 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,58 @@
+import { useState } from "react";
+import {data} from "../../../data"
+
 const UserChallenge = () => {
+
+  const [name,setName] = useState('')
+
+  const [users,setUsers] = useState(data)
+
+  function getInput(e){
+    setName(e.target.value)
+  }
+
+  function addUser(e){
+    e.preventDefault()
+    if(!name) {
+      alert('Field can not be empty') 
+      return;
+    }
+    setUsers(old=>[...old,{id:Math.floor(Math.random() * 99999),name}])
+    setName('')
+  }
+
   return (
     <div>
-      <form className='form'>
+      <form onSubmit={addUser} className='form'>
         <h4>Add User</h4>
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input 
+          value={name} 
+          onChange={getInput}
+          type='text' 
+          className='form-input' 
+          id='name' />
         </div>
 
         <button type='submit' className='btn btn-block'>
           submit
         </button>
       </form>
-      {/* render users below */}
+      {users.length>0 && <h4>users</h4>}
+      {users.map((value)=>{
+        return <div key={value.id} >
+          {value.name}
+                <button 
+                  className="btn" 
+                  style={{marginLeft:"1em"}}
+                  onClick={()=>{setUsers(user=>user.filter(user=>user.id!==value.id))}}
+                  >Remove
+                </button>
+              </div>
+      })}
     </div>
   );
 };
--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,41 @@
+import { useState } from "react";
+import text from "./data";
+
+const data = text;
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text,setText] = useState([])
+
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  };
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setText(data.slice(0,count))
+ 
+
+  }
+
+  return (
+    <section className="section-center">
+      <h4>Tired of boring lorem?</h4>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          id="amount"
+          type="number"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={handleChange}
+        />
+        <button className="btn" type="submit"> Generate </button>
+      </form>
+      {text.map(text=><div style={{marginBottom:'2em'}} key={text}>{text}</div>)}
+    </section>
+  );
 };
 export default App;
